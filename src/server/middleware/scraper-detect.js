55cb553474b0677f8c19dc1dfc082ca9d44bb847
King, Jake (Apprentice Software Developer) <Jake.King@sky.uk>
2017-11-16 21:46:42 +0000

    Added scraper for slack

--- src/server/middleware/scraper-detect.js
+++ src/server/middleware/scraper-detect.js
@@ -1,10 +1,11 @@
 export default async (ctx, next) => {
   const fbRegex = /^FacebookExternalHit\/.*?/i;
   const twRegex = /^TwitterBot\/.*?/i;
+  const slRegex = /^Slackbot-LinkExpanding\/.*?/i;
 
   const userAgent = ctx.request.header['user-agent'];
 
-  if (fbRegex.test(userAgent) || twRegex.test(userAgent)) {
+  if (fbRegex.test(userAgent) || twRegex.test(userAgent) || twRegex.test(slRegex)) {
     return ctx.redirect('/tags');
   }
 
