6e5be0adc801c8f09dd1960e8d61bf13af65355c
King, Jake (Apprentice Software Developer) <Jake.King@sky.uk>
2017-11-16 20:50:20 +0000

    Meta tags

--- src/server/view/index.js
+++ src/server/view/index.js
@@ -3,8 +3,13 @@ export default (html, preloadedState) => {
     <!doctype html>
     <html>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">    
-        <title>Spotboard</title>
+        <meta property="og:title" content="Spotboard - A dashboard for Spotify"/>
+        <meta property="og:url" content="https://www.spotboard.pro/"/>
+        <meta property="og:site_name" content="Spotboard"/>
+        <meta name="description" content="A dashboard for Spotify that will show you your music tastes from now to way back">
+        <meta name="author" content="Jake King">        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Spotboard - A dashboard for Spotify</title>
         <link rel="stylesheet" type="text/css" href="/static/bundle.css">        
       </head>
       <body>
